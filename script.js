4cfeb6e523701c5927dcad651eea0008c9a984bb
dominiclai1983 <>
2021-11-03 13:00:57 +0000

    complete a function to get the content from API, fixed input field.

--- script.js
+++ script.js
@@ -1,17 +1,27 @@
 //function to get the data of the api with key 179
-var getDataFromAPI = function(){
-  $.ajax({
-  type: 'GET',
-  url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=179',
-  dataType: 'json',
-  success: function (response, textStatus) {
-    console.log(response);
-    // response is a parsed JavaScript object instead of raw JSON
-  },
-  error: function (request, textStatus, errorMessage) {
-    console.log(errorMessage);
-  }
-});
+  var getDataFromAPI = function (){
+    $.ajax({
+    type: 'GET',
+    url: 'https://altcademy-to-do-list-api.herokuapp.com/tasks?api_key=1',
+    dataType: 'json',
+    success: function (response) {
+      response.tasks.forEach(function(index){
+        console.log(index.content);
+        var newContent = index.content;
+        $(".2nd-row").after('<div class="row my-2 align-items-center">'+
+        '<div class="col-3 form-check mx-2">'+
+        '<input class="form-check-input" type="checkbox" value="" id="defaultCheck1">'+'</div>'+
+        '<div class="col-9 ml-3">'+
+        newContent +
+        '</div>'+
+        '</div>'
+        );
+      })
+    },
+    error: function (request, textStatus, errorMessage) {
+      console.log(errorMessage);
+    }
+  });
 }
 
 $(document).ready(function(){
@@ -20,14 +30,18 @@ $(document).ready(function(){
 
   $(".add-html").disabled = true;
 
-
   $(".form-control").on('input', function(){
     $(".add-html").disabled = false;
     $(".add-item").html("Add");
   });
 
   $(".form-control").blur(function(){
-    $(".add-html").disabled = true;
-    $(".add-item").html("Input");
+    if(!$('.form-control').val()){
+      $(".add-html").disabled = true;
+      $(".add-item").html("Input");
+    }
   });
+
 });
+
+      
